0b2915e3c88d02b055c3c1af4118a2bebe63e234
Chris Behan <chrisbehan1997@gmail.com>
2020-10-18 17:36:27 -0500

    Adds intro paragraph.

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,27 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p className={utilStyles.centerText}>
-          Reader, writer, and data transformer.
+          Communicator and Data Transformer.
+        </p>
+      </section>
+      <section className={utilStyles.headingSm}>
+        <p className={utilStyles.introText}>
+          I would describe myself as a passionate neurotic who struggles with
+          moderation. I currently transform data for{" "}
+          <a href="https://aws.amazon.com/" className={utilStyles.orangeAnchor}>
+            AWS
+          </a>
+          . Technology fascinates me. I believe there is no difference between
+          magic and enough layers of abstraction. I spend too much time
+          pondering philosophical questions. I love to both play and watch
+          basketball (<a href="https://www.nba.com/thunder/">Thunder up!</a>).{" "}
+          <a
+            className={utilStyles.redAnchor}
+            href="https://open.spotify.com/album/4wExFfncaUIqSgoxnqa3Eh?si=UmkV7iSCRZuob0szOKBohA"
+          >
+            Led Zeppelin
+          </a>{" "}
+          is my favorite band of all time.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
